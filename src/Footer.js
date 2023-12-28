2a13389f667cb391c1af91742f053613495637e9
BIKASH MALU <bikashmalu1@gmail.com>
2023-12-29 04:01:40 +0530

    add

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='footer'>
-    <h5>All Rights Reserved by Bikash Malu</h5>  
+    <h5>All Rights Reserved by Bikash</h5>  
     </div>
   )
 }
